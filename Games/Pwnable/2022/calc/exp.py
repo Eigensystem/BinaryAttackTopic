d649d1d5ad8a3510f32ee05a15ba6d703305fe83
Eigensystem <eigensystem1318@gmail.com>
2022-05-21 23:45:05 +0800

    add pwnable 2022

---
+++ Games/Pwnable/2022/calc/exp.py
@@ -0,0 +1,17 @@
+from pwn import *
+context(arch = "amd64", os = "Linux", log_level = "Debug")
+io = process("./calc")
+# io = remote("124.70.130.92", 60001)
+# gdb.attach(io)
+
+s       = lambda data               :io.send(data)
+sa      = lambda delim,data         :io.sendafter(str(delim), data)
+sl      = lambda data               :io.sendline(data)
+sla     = lambda delim,data         :io.sendlineafter(str(delim), data)
+r       = lambda num=4096           :io.recv(num)
+ru      = lambda delims             :io.recvuntil(delims)
+rl      = lambda                    :io.recvline()
+itr     = lambda                    :io.interactive()
+uu32    = lambda data               :u32(data.ljust(4, b'\x00'))
+uu64    = lambda data               :u64(data.ljust(8, b'\x00'))
+
